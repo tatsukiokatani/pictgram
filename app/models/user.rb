f4d2ad66caf835772769fcb61c085ca633e7643d
”tatsukiokatani” <tatsuki1020ok@yahoo.co.jp>
2020-12-01 06:00:50 +0000

    password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8, maximum: 32}
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
   validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
