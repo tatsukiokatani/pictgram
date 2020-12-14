4148013a1fec55604a8699df33ff4627ae2bd93a
”tatsukiokatani” <tatsuki1020ok@yahoo.co.jp>
2020-12-14 03:41:11 +0000

    heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-   %w(jpg jpeg png)
+   %w(jpg jpeg gif png)
   end
   
   def size_range
